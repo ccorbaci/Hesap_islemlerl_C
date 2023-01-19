dbda05219cc91ce9eaec7c647bd992521e87065c
Cüneyt Çorbacı <ccorbaci@gmail.com>
2023-01-19 13:43:58 +0300

    Günlük limit eklendi.

--- main.c
+++ main.c
@@ -14,24 +14,33 @@ int main(int argc, char *argv[]) {
 	float bakiye=(float) (rand() % 2000);
 	float cekilecek=0;
 	//Rastgele üretilen sayýyý max. 2000 de tutmak için ise rand() fonk ile üretilen sayý degeri,2000 sayýsý ile mod iþlemine tabi tutulur.
-	//Örneðin; rand fonk. 4581 üretecek olursa 4581/2000 iþleminin sonucundan kalan atanýr.Ýþlemin sonucu olarak 581 deðerini alýr.Bu bölme iþleminde kalan daima bölünenden küçük olacaktýr.
+	//Örneðin; rand fonk. 4581 üretecek olursa 4581/2000 iþleminin sonucundan kalan atanýr.Ýþlemin sonucu olarak 581 deðerini alýr.
+	//Bu bölme iþleminde kalan daima bölünenden küçük olacaktýr.
+	float limit=500;
+	//cekilecek para için günlük limit
 	
 	printf("*****Bankamiza Hos Geldiniz*****\n");
 	printf("Su anki bakiyeniz %.2f\n",bakiye);
 	printf("*****Lutfen Cekmek Istediginiz miktari giriniz..*****\n");
 	scanf("%f",&cekilecek);
 	
-	if(cekilecek<=bakiye)
+	if(cekilecek<=bakiye && cekilecek <= limit)
+	//Bu þart bize "Eðer kullanýcýnýn hesabýnda yeterli para varsa (if) ve çekilmek istenen para limiti aþmýyorsa(&&) bloðu çalýstýr.
 	//Eðer çekilecek sayý bakiyeden küçük ise aþaðýdaki blok devreye girecek, Kalan mikrarý da kullanýcý ile paylaþacaktýr.
 	{
 		bakiye=bakiye-cekilecek;
 		printf("Cekilen Para Miktari: %.2f\n",cekilecek);
 		printf("Kalan Para Miktari: %.2f\n",bakiye);
 	}
-	
+	else
+	//ýf bloðundaki kosul karþýlanmadýðýnda else blogu calýsarak bakiye yetersiz uyarýsý verecektir.
+	{
+		printf("Hesabinizda Bunun Icin Yeterli Bakiye Bulunmuyor\n");
+		printf("Veya cekmek istediginiz rakam %.2f den daha fazla\n",limit);
+	}
 	
 	
 		
-	printf("Program Sonlandi.Tesekkurler");
+	printf("Program Sonlandi.\nTesekkurler");
 	return 0;
 }
